ee4f90e5732171e6fe332b254f8d9049b40c1f63
Dongsusin <auroratime020715@gmail.com>
2025-04-28 17:01:51 +0900

    유튜브추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,6 +15,7 @@ const Header = () => {
           <li>
             <a href="https://sena.netmarble.com/ko">사전등록</a>
             <a href="https://game.naver.com/lounge/sena_rebirth/home">라운지</a>
+            <a href="https://www.youtube.com/@sena_rebirth">유튜브</a>
           </li>
         </ul>
       </div>
